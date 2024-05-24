4a43e167a06e29808d040cf3dc9475beaaec18c0
phonglam <lamphong1507@gmail.com>
2024-05-24 07:01:35 +0700

    Update CSS

--- check.js
+++ check.js
@@ -44,12 +44,16 @@ function check() {
   var month = document.getElementById("month").value;
   var year = document.getElementById("year").value;
 
-  if (year > MAX_VALID_YR || year < MIN_VALID_YR) {
-    alert("Input data for Year is out of range");
-  } else if (month < 1 || month > 12) {
-    alert("Input data for Month is out of range");
-  } else if (day < 1 || day > 31) {
+  if (day < 1 || day > 31 || isNaN(month)) {
     alert("Input data for Day is out of range");
+  }
+
+  if (month < 1 || month > 12 || isNaN(month)) {
+    alert("Input data for Month is out of range");
+  }
+
+  if (year > MAX_VALID_YR || year < MIN_VALID_YR || isNaN(year)) {
+    alert("Input data for Year is out of range");
   } else {
     isValidDate(day, month, year)
       ? alert(+day + "/" + month + "/" + year + " is a Valid Date")
